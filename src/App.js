af024cd8afefad1a409f86d536e7b67153a661f1
bchoi8 <bchoi@totalwine.com>
2019-10-19 20:43:23 -0400

    add TodoList

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import Todos from './components/Todos';
+
+
+import AddTodo from './components/AddTodo';
+import './css/base.css';
+
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Do homework',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Start project',
+        completed: false
+      }
+    ]
+  };
+
+  markComplete = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
+
+  delTodo = id => {
+    this.setState({
+      todos: [...this.state.todos.filter(todo => todo.id !== id)]
+    });
+  };
+
+  addTodo = title => {
+    const newTodo = {
+      id: this.state.todos.length + 1,
+      title,
+      completed: false
+    };
+
+    this.setState({ todos: [...this.state.todos, newTodo] });
+  };
+
+  render() {
+    return (
+      <div>
+        <div className='title'>
+            <h1 className='headerText'>To Do List</h1>
+        </div>
+        <AddTodo addTodo={this.addTodo} />
+        <Todos
+          todos={this.state.todos}
+          markComplete={this.markComplete}
+          delTodo={this.delTodo}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
